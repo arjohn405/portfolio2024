535d4e6b30a5587ebf95f8533b1e1e40d7a386ff
arjohn405 <acapucion123@gmail.com>
2024-12-31 17:19:13 +0800

    fixed banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Banner({ imageUrl }) {
   return (
     <div className="banner" style={{ backgroundImage: `url(${imageUrl})` }}>
-      {/* The background image is set via inline style */}
+      <div className="banner-content">
+        {/* You can add any content here if needed */}
+      </div>
     </div>
   );
 }
